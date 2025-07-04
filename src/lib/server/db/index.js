a176f03442f87658c848f875288c8f5a63d9dab6
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2025-07-04 23:18:05 +0200

    chore: adjust the env part of the workflow

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -1,16 +1,16 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
-import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '$env/static/private';
+import * as env from '$env/static/private';
 
-if (!DB_HOST) throw new Error('DB_HOST is not set');
+if (!env.DB_HOST) throw new Error('DB_HOST is not set');
 
 const client = mysql.createPool({
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    host: env.DB_HOST,
+    port: Number(env.DB_PORT),
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
     waitForConnections: true // Connection limit and queue limit are 10 / 0 by default
 });
 
